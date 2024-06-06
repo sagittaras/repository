0d6295f3025c24b66ebffbe5ca33d3a4f263183a
Jan Zechovský <email@zechy.cz>
2024-06-06 18:20:36 +0200

    PageCollection is described as read only collection.

--- Sagittaras.Repository/Queries/Find/Pagination/IPagedCollection.cs
+++ Sagittaras.Repository/Queries/Find/Pagination/IPagedCollection.cs
@@ -1,22 +1,21 @@
 ﻿using System.Collections.Generic;
 
-namespace Sagittaras.Repository.Queries.Find.Pagination
+namespace Sagittaras.Repository.Queries.Find.Pagination;
+
+public interface IPagedCollection : IReadOnlyCollection<object>
 {
-    public interface IPagedCollection : IEnumerable<object>
-    {
-        /// <summary>
-        /// Total data count in the dataset.
-        /// </summary>
-        int Total { get; set; }
+    /// <summary>
+    /// Total data count in the dataset.
+    /// </summary>
+    int Total { get; set; }
         
-        /// <summary>
-        /// How many items are in the current page.
-        /// </summary>
-        int Limit { get; set; }
+    /// <summary>
+    /// How many items are in the current page.
+    /// </summary>
+    int Limit { get; set; }
         
-        /// <summary>
-        /// How many items are skipped from the beginning of the dataset.
-        /// </summary>
-        int Offset { get; set; }
-    }
+    /// <summary>
+    /// How many items are skipped from the beginning of the dataset.
+    /// </summary>
+    int Offset { get; set; }
 }
--- Sagittaras.Repository/Queries/Find/Pagination/PagedCollection.cs
+++ Sagittaras.Repository/Queries/Find/Pagination/PagedCollection.cs
@@ -1,38 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-namespace Sagittaras.Repository.Queries.Find.Pagination
+namespace Sagittaras.Repository.Queries.Find.Pagination;
+
+/// <summary>
+/// Collection describing the result of a paginated query.
+/// </summary>
+/// <typeparam name="TData">Type of data in collection.</typeparam>
+public class PagedCollection<TData> : IPagedCollection
 {
-    /// <summary>
-    /// Collection describing the result of a paginated query.
-    /// </summary>
-    /// <typeparam name="TData">Type of data in collection.</typeparam>
-    public class PagedCollection<TData> : IPagedCollection
-    {
-        /// <inheritdoc />
-        public int Total { get; set; }
+    /// <inheritdoc />
+    public int Total { get; set; }
 
-        /// <inheritdoc />
-        public int Limit { get; set; }
+    /// <inheritdoc />
+    public int Limit { get; set; }
 
-        /// <inheritdoc />
-        public int Offset { get; set; }
+    /// <inheritdoc />
+    public int Offset { get; set; }
         
-        /// <summary>
-        /// Enumerable of data in the current page.
-        /// </summary>
-        public IEnumerable<TData> Data { get; set; } = new List<TData>();
+    /// <summary>
+    /// Enumerable of data in the current page.
+    /// </summary>
+    public ICollection<TData> Data { get; set; } = new List<TData>();
+        
+    /// <inheritdoc />
+    public int Count => Data.Count;
 
-        /// <inheritdoc />
-        public IEnumerator<object> GetEnumerator()
-        {
-            return (IEnumerator<object>) Data.GetEnumerator();
-        }
+    /// <inheritdoc />
+    public IEnumerator<object> GetEnumerator()
+    {
+        return (IEnumerator<object>) Data.GetEnumerator();
+    }
 
-        /// <inheritdoc />
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+    /// <inheritdoc />
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 }
