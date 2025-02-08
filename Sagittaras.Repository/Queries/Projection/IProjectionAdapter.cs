9a602ee610757f2be0a6809f580a8b1dd7073825
Jan Zechovský <email@zechy.cz>
2025-02-08 13:36:43 +0100

    docs(projection): improve XML documentation for `IProjectionAdapter` and `NotSupportedProjectionAdapter`

--- Sagittaras.Repository/Queries/Projection/IProjectionAdapter.cs
+++ Sagittaras.Repository/Queries/Projection/IProjectionAdapter.cs
@@ -2,13 +2,17 @@
 
 namespace Sagittaras.Repository.Queries.Projection
 {
+    /// <summary>
+    ///     Provides functionality to adapt a queryable source for projecting its elements to a different type.
+    /// </summary>
     public interface IProjectionAdapter
     {
         /// <summary>
-        /// Projects the queryable to target destionation.
+        ///     Projects the elements of a queryable source to a different type.
         /// </summary>
-        /// <typeparam name="TDestination">The target destination to which the queryable should be projected.</typeparam>
-        /// <returns></returns>
-        IQueryable<TDestination> ProjectTo<TDestination>(IQueryable queryable);
+        /// <typeparam name="TResult">The target type to which elements are projected.</typeparam>
+        /// <param name="queryable">The queryable data source to be projected.</param>
+        /// <returns>A queryable of the target type containing the projected elements.</returns>
+        IQueryable<TResult> ProjectTo<TResult>(IQueryable queryable);
     }
 }
--- Sagittaras.Repository/Queries/Projection/NotSupportedProjectionAdapter.cs
+++ Sagittaras.Repository/Queries/Projection/NotSupportedProjectionAdapter.cs
@@ -4,10 +4,15 @@ using System.Linq;
 namespace Sagittaras.Repository.Queries.Projection
 {
     /// <summary>
-    /// Default implementation of projection adapter without support.
+    ///     An implementation of <see cref="IProjectionAdapter"/> that does not support entity projection.
     /// </summary>
+    /// <remarks>
+    ///     This adapter throws a <see cref="NotSupportedException"/> if attempted to perform projection,
+    ///     encouraging users to provide custom implementations for specific entity projections.
+    /// </remarks>
     public class NotSupportedProjectionAdapter : IProjectionAdapter
     {
+        /// <inheritdoc />
         public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable queryable)
         {
             throw new NotSupportedException($"For entities projection create custom implementation of {nameof(IProjectionAdapter)}");
